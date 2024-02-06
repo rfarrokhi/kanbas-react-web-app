635563c83be7f90811de00916b9004fa3db1634e
Reza Farrokhi Saray <61975123+rfarrokhi@users.noreply.github.com>
2024-02-05 23:54:19 -0500

    Assignment2
    requirements for assignment 2 are added

--- public/NavigationInjection.js
+++ public/NavigationInjection.js
@@ -1,51 +1,114 @@
-var KanbasNavigationPath = '/Kanbas/Navigation/index.html';
-var KanbasNavigationTargetElementId = 'kanbas-navigation';
+const Navigations = [
+    {"kanbas-navigation": "/Kanbas/Navigations/kanbas_navigation.html"},
+    {"course-navigation": "/Kanbas/Navigations/course_navigation.html"},
+    {"account-navigation": "/Kanbas/Navigations/account_navigation.html"},
+]
 
-var CourseNavigationPath = '/Kanbas/Courses/Navigation/index.html';
-var CourseNavigationTargetElementId = 'course-navigation';
+const KanbasNavigationPathList = [
+    {"Account": "/Kanbas/Account/Profile"},
+    {"Dashboard": "/Kanbas/Dashboard"},
+    {"Courses": "/Kanbas/Courses"},
+]
 
-var AccountNavigationPath = '/Kanbas/Account/Navigation/index.html';
-var AccountNavigationTargetElementId = 'account-navigation';
+const CoursesNavigationPathList = [
+    {"Home": "/Kanbas/Courses/Home"},
+    {"Assignments": "/Kanbas/Courses/Assignments"},
+    {"Grades": "/Kanbas/Courses/Grades"},
+    {"Settings": "/Kanbas/Courses/Settings"},
+]
 
 /**
  * Injects the HTML content into the target element.
  * @param {string} htmlContent The HTML content to inject.
  * @param {string} targetElementId The ID of the target element.
  */
-function injectHTML(htmlContent, targetElementId) {
+async function injectHTML(htmlContent, targetElementId) {
     if (!targetElementId || targetElementId.length === 0) {
         console.error('Target element ID is required.');
         return;
     }
 
-    let targetElement = document.getElementById(targetElementId);
-    if (targetElement) {
-        targetElement.innerHTML = htmlContent;
-    } else {
-        console.error('Target element not found.');
+    return new Promise((resolve) => {
+        let targetElement = document.getElementById(targetElementId);
+
+        if (targetElement) {
+            targetElement.innerHTML = htmlContent;
+            console.log("Injected navigation into " + targetElementId)
+        }
+        else {
+            console.error(`Target element with ID '${targetElementId}' not found.`);
+        }
+
+        resolve();
+    });
+}
+
+/**
+ * Injects the navigation sidebars into the target elements.
+ * @param {Array} navigationList The list of navigations to inject.
+ */
+async function injectNavigationSidebars(navigationList) {
+    return new Promise(async (resolve) => {
+
+        for (let index = 0; index < navigationList.length; index++) {
+
+            let navigation = navigationList[index];
+            let key = Object.keys(navigation)[0];
+            let value = navigation[key];
+
+            try {
+                let response = await fetch(value);
+                let data = await response.text();
+
+                // Inject the HTML of the navigation into the target element.
+                await injectHTML(data, key);
+
+                // Resolve the promise when the last navigation is injected.
+                if (index === navigationList.length - 1) {
+                    console.log("All navigations injected.");
+                    resolve();
+                }
+
+            }
+            catch (error) {
+                console.error(`Failed to fetch ${key}: ${error}`);
+            }
+
+        }
+
+    });
+}
+
+/**
+ * Sets the active class for the current navigation item in the given navigation sidebar.
+ * @param {Array} navigationPathList The list of navigation paths.
+ * @param {string} activeClassName The name of the active class.
+ * @param {string} parentElementId The ID of the element containing the navigation items.
+ */
+function setActiveNavigationItem(navigationPathList, activeClassName, parentElementId) {
+    let currentPath = window.location.pathname;
+    let pathWithoutFilename = currentPath.replace(/\/[^\/]*$/, '');
+
+    for (let i = 0; i < navigationPathList.length; i++) {
+
+        let pathName = Object.keys(navigationPathList[i])[0];
+        let path = navigationPathList[i][pathName];
+
+        if (pathWithoutFilename.toLowerCase().includes(path.toLowerCase())) {
+
+            let element = document.querySelector(`#${parentElementId} a.nav-link[href*="${path}"]`);
+            element && element.classList.add(activeClassName);
+
+            return;
+        }
     }
 }
 
-
-
-fetch(KanbasNavigationPath)
-    .then(response => response.text())
-    .then(htmlContent => {
-        injectHTML(htmlContent, KanbasNavigationTargetElementId);
+// Inject the navigations into the target elements.
+injectNavigationSidebars(Navigations)
+    .then(() => {
+        console.log("Setting the appropriate class for active navigation items...")
+        // Add active class to the current navigation items
+        setActiveNavigationItem(KanbasNavigationPathList, "nav-active", "kanbas-navigation");
+        setActiveNavigationItem(CoursesNavigationPathList, "active", "course-navigation");
     })
-    .catch(error => console.error('Error fetching HTML file for Kanbas Navigation:', error));
-
-fetch(CourseNavigationPath)
-    .then(response => response.text())
-    .then(htmlContent => {
-        injectHTML(htmlContent, CourseNavigationTargetElementId);
-    })
-    .catch(error => console.error('Error fetching HTML file for Course Navigation:', error));
-
-
-fetch(AccountNavigationPath)
-    .then(response => response.text())
-    .then(htmlContent => {
-        injectHTML(htmlContent, AccountNavigationTargetElementId);
-    })
-    .catch(error => console.error('Error fetching HTML file for Account Navigation:', error));
