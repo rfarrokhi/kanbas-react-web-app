9001e0f3cd3ff4788cb386bb62cb8b6066d933b2
Mehrdad Moradi <61975123+rfarrokhi@users.noreply.github.com>
2024-04-03 15:54:30 -0500

    Update WorkingWithObjects.tsx
    Improved handling of updating module and assignment

--- src/Labs/a5/WorkingWithObjects.tsx
+++ src/Labs/a5/WorkingWithObjects.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {BASE_API_URL} from "./client";
+const ASSIGNMENT_URL = `${BASE_API_URL}/assignment`;
+const MODULE_URL = `${BASE_API_URL}/module`;
 
 function WorkingWithObjects() {
     
@@ -12,8 +14,6 @@ function WorkingWithObjects() {
       completed: false, 
       score: 0,
     });
-    const ASSIGNMENT_URL = `${BASE_API_URL}/assignment`;
-    console.log(ASSIGNMENT_URL);
 
     const [module, setModule] = useState({
       id: 5610, 
@@ -21,28 +21,71 @@ function WorkingWithObjects() {
       description: " NodeJS on Web Development",
       course: "CS5610 Web Development",
     });
-    const MODULE_URL = BASE_API_URL + "module"
 
     const fetchAssignment = async () => {
       const response = await axios.get(`${ASSIGNMENT_URL}`);
       setAssignment(response.data);
     };
+
+    const fetchModule = async () => {
+        const response = await axios.get(`${MODULE_URL}`);
+        setModule(response.data);
+    }
     
-    const updateTitle = async () => {
+    const updateAssignmentTitle = async () => {
       const response = await axios
         .get(`${ASSIGNMENT_URL}/title/${assignment.title}`);
-      setAssignment(response.data);
     };
+
+    const updateAssignmentScore = async () => {
+        const response = await axios
+            .get(`${ASSIGNMENT_URL}/score/${assignment.score}`);
+    }
+
+    const updateAssignmentCompleted = async () => {
+        const response = await axios
+            .get(`${ASSIGNMENT_URL}/completed/${assignment.completed}`);
+    }
+
+    const updateAssignment = async () => {
+        await updateAssignmentTitle();
+        await updateAssignmentScore();
+        await updateAssignmentCompleted();
+        alert("Assignment updated");
+    }
+
+    const updateModuleName = async () => {
+        const response = await axios
+            .get(`${MODULE_URL}/name/${module.name}`);
+    }
+
+    const updateModuleDescription = async () => {
+        const response = await axios
+            .get(`${MODULE_URL}/description/${module.description}`);
+    }
+
+    const updateModule = async () => {
+        await updateModuleName();
+        await updateModuleDescription();
+        alert("Module updated");
+    }
+
+
+
+
     useEffect(() => {
       fetchAssignment();
+      fetchModule();
     }, []);
   
     
     return (
       <div>
         <h3>Working With Objects</h3>
-        <h3>Modifying Properties</h3>
+        <h3>Modifying Properties of assignment</h3>
+          <label htmlFor={"assignmentTitle"}>Assignment Title</label>
         <input
+            id={"assignmentTitle"}
           onChange={(e) =>
             setAssignment({
               ...assignment,
@@ -53,54 +96,55 @@ function WorkingWithObjects() {
           className="form-control mb-2"
           type="text"
         />
-        <button onClick={updateTitle} className="w-100 btn btn-primary mb-2">
-          Update Title to: {assignment.title}
-        </button>
-        <button onClick={fetchAssignment} className="w-100 btn btn-danger mb-2">
-          Fetch Assignment
+        <label htmlFor={"assignmentScore"}>Assignment Score</label>
+        <input
+            id={"assignmentScore"}
+          onChange={(e) =>
+            setAssignment({
+              ...assignment,
+              score: Number(e.target.value),
+            })
+          }
+          value={assignment.score}
+          className="form-control mb-2"
+          type="text"
+        />
+        <label htmlFor={"assignmentCompleted"}>Assignment Completed</label>
+        <input
+            id={"assignmentCompleted"}
+          onChange={(e) =>
+            setAssignment({
+              ...assignment,
+              completed: Boolean(e.target.value),
+            })
+          }
+          checked={assignment.completed}
+          type="checkbox"
+        />
+        <button onClick={updateAssignment} className="w-100 btn btn-primary mb-2">
+          Update Assignment
         </button>
   
-        <h4>Modifying Properties</h4>
-        <a
-          href={`${ASSIGNMENT_URL}/title/${assignment.title}`}
-          className="btn btn-primary me-2 float-end"
-        >
-          Update Title
-        </a>
-        <input
-          type="text"
-          onChange={(e) =>
-            setAssignment({ ...assignment, title: e.target.value })
-          }
-          value={assignment.title}
-          className="form-control mb-2 w-75"
-        />
+        <h3>Modifying Properties of Module</h3>
 
 <div className="form-control mb-2">
         <div className="row">
           <div className="col-sm-2">
-          <input type="text" className="form-control mb-2"
+              <label htmlFor={"moduleName"}>Module Name</label>
+          <input id={"moduleName"} type="text" className="form-control mb-2"
             onChange={(e) => setModule({ ...module,
                 name: e.target.value })}
             value={module.name}/>
           </div>
-          <div className="col-sm-2">
-          <a href={`${MODULE_URL}/name/${module.name}`} className="btn btn-danger">
-            Update Module Name
-          </a>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-2">
-          <input type="text" className="form-control"
+            <label htmlFor={"moduleDescription"}>Module Description</label>
+            <input id={"moduleDescription"} type="text" className="form-control mb-2"
             onChange={(e) => setModule({ ...module,
                 description: e.target.value })}
             value={module.description}/>
-          </div>
-          <div className="col-sm-2">
-          <a href={`${MODULE_URL}/description/${module.description}`} className="btn btn-danger">
-            Update Description
-          </a>
+          <div className="w-100 btn btn-primary mb-2">
+          <button onClick={updateModule} className="btn btn-primary">
+            Update Module
+            </button>
           </div>
         </div>
       </div>
@@ -112,18 +156,23 @@ function WorkingWithObjects() {
         >
           Get Assignment
         </a>
+
+        <a
+          href={`${BASE_API_URL}/module`}
+          className="btn btn-danger"
+        >
+            Get Module
+        </a>
         <h4>Retrieving Properties</h4>
         <a
           className="btn btn-primary me-2"
           href={`${BASE_API_URL}/assignment/title`}
         >
-          Get Title
+          Get Assignment Title
         </a>
-
         <a href={`${BASE_API_URL}/module/name`} className="btn btn-danger">
           Get Module Name
         </a>
-
       </div>
     );
   }
